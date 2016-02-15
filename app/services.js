8bb3017014e0bfbc5fabfd9452991d089bf655ce
jrpespisa <jrpespisa@verizon.net>
2016-02-15 10:34:57 -0500

    added f1 api integration

---
+++ app/services.js
@@ -0,0 +1,12 @@
+angular.module("F1FeederApp.service", [])
+  .factory("ergastAPIservice", function($http) {
+    var ergastAPI = {};
+
+    ergastAPI.getDrivers = function() {
+      return $http({
+        method: 'JSONP',
+        url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+    return ergastAPI;
+  });
