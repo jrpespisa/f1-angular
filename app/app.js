1914f1076df68fdca463735e29e79cdad45c5d7b
jrpespisa <jrpespisa@verizon.net>
2016-02-16 13:42:14 -0500

    added partials and routes

--- app/app.js
+++ app/app.js
@@ -1,4 +1,11 @@
 angular.module('F1FeederApp', [
-  'F1FeederApp.controller',
-  'F1FeederApp.service'
-]);
+  'F1FeederApp.services',
+  'F1FeederApp.controllers',
+  'ngRoute'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+	when("/drivers", {templateUrl: "partials/drivers.html", controller: "driversController"}).
+	when("/drivers/:id", {templateUrl: "partials/driver.html", controller: "driverController"}).
+	otherwise({redirectTo: '/drivers'});
+}]);
--- app/services.js
+++ app/services.js
@@ -1,5 +1,6 @@
-angular.module("F1FeederApp.service", [])
-  .factory("ergastAPIservice", function($http) {
+angular.module('F1FeederApp.services', [])
+  .factory('ergastAPIservice', function($http) {
+
     var ergastAPI = {};
 
     ergastAPI.getDrivers = function() {
@@ -8,5 +9,20 @@ angular.module("F1FeederApp.service", [])
         url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
       });
     }
+
+    ergastAPI.getDriverDetails = function(id) {
+      return $http({
+        method: 'JSONP',
+        url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+
+    ergastAPI.getDriverRaces = function(id) {
+      return $http({
+        method: 'JSONP',
+        url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
+      });
+    }
+
     return ergastAPI;
   });
